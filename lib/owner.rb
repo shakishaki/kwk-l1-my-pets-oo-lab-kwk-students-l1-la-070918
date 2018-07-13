722fd400aa0441542fd822d4b0260681ffc0d074
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 16:33:53 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
   # code goes here
+  attr_accessor :cat, :dog, :fish
+  def initialize(cat, dog, fish)
+    @cat = cat
+    @dog = dog
+    @fish = fish
+  end
+end
+    
+  cat_one = Cat.new("kiwi", "sassy")
+  puts cat_one.name 
+  puts cat_one.mood
 
+  dog_one = Dog.new("")
