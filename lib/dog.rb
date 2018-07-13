1ce2a6608886c56f4e594d37a4006de063158156
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 16:31:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
   # code goes here
+  attr_accessor :name, :mood
+  def initialize(name, mood)
+    @name = name
+    @mood = mood
+  end
 end
+    
+  dog_one = Dog.new("Cheddar", "hungry")
+  puts dog_one.name 
+  puts dog_one.mood
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,12 @@
 class Fish
   # code goes here
+  attr_accessor :name, :mood
+  def initialize(name, mood)
+    @name = name
+    @mood = mood
+  end
 end
+    
+  fish_one = Fish.new("Dory", "bubbly")
+  puts fish_one.name 
+  puts fish_one.mood
