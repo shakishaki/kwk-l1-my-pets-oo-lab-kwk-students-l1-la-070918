f550ebad9f9398e9f5aa0a9f398f8f1fec82570f
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 16:41:23 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
   # code goes here
- 
+  attr_accessor :name, :mood
+  def initialize(name, mood)
+    @name = name
+    @mood = mood
+  end
+end
+    
+  cat_one = Cat.new("kiwi", "sassy")
+  puts cat_one.name 
+  puts cat_one.mood
